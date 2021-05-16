0d21a51bf6279a83486c1847442aea16bbcd59f2
Gary <39154346+oniyou@users.noreply.github.com>
2021-05-17 04:54:33 +0800

    Update Youth_Read.js

--- Youth_Read.js
+++ Youth_Read.js
@@ -244,8 +244,8 @@ function batHost(api, body) {
 
 function readTime() {
     return new Promise((resolve, reject) => {
-        $.post(batHost('user/stay.json', timebodyVal), (error, resp, data) => {
-            $.log('\ntimebodyVal: ' + timebodyVal + '\n')
+        $.post(batHost('user/stay.json', articlebody), (error, resp, data) => {
+            $.log('\ntimebodyVal: ' + articlebody + '\n')
             $.log(data)
             let timeres = JSON.parse(data)
             if (timeres.error_code == 0) {
